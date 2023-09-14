fa3b10b7de6a1680a51f8fb468996e2d094648da
bheston1 <bheston721@gmail.com>
2023-09-14 03:14:40 -0400

    refactoring

---
+++ MathGame/Helpers.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathGame
+{
+    internal class Helpers
+    {
+        internal static string GetName()
+        {
+            Console.Clear();
+            Console.Write("Please enter your name: ");
+            var nameInput = Console.ReadLine();
+            while (string.IsNullOrEmpty(nameInput))
+            {
+                Console.Write("\nName cannot be empty.\nEnter your name: ");
+                nameInput = Console.ReadLine();
+            }
+            return nameInput;
+        }
+
+        internal static void TitleScreen()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("███╗   ███╗ █████╗ ████████╗██╗  ██╗     ██████╗  █████╗ ███╗   ███╗███████╗\r\n████╗ ████║██╔══██╗╚══██╔══╝██║  ██║    ██╔════╝ ██╔══██╗████╗ ████║██╔════╝\r\n██╔████╔██║███████║   ██║   ███████║    ██║  ███╗███████║██╔████╔██║█████╗  \r\n██║╚██╔╝██║██╔══██║   ██║   ██╔══██║    ██║   ██║██╔══██║██║╚██╔╝██║██╔══╝  \r\n██║ ╚═╝ ██║██║  ██║   ██║   ██║  ██║    ╚██████╔╝██║  ██║██║ ╚═╝ ██║███████╗\r\n╚═╝     ╚═╝╚═╝  ╚═╝   ╚═╝   ╚═╝  ╚═╝     ╚═════╝ ╚═╝  ╚═╝╚═╝     ╚═╝╚══════╝\r\n");
+            Console.ResetColor();
+            Console.Write("Press ENTER to play");
+            PressEnter();
+        }
+
+        internal static void PressEnter()
+        {
+            ConsoleKeyInfo key;
+            do
+            {
+                key = Console.ReadKey(true);
+            } while (key.Key != ConsoleKey.Enter);
+        }
+    }
+}
