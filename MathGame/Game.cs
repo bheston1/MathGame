9d358aa86519a3fbb5b5f14f02787b5fdd0ec068
bheston1 <bheston721@gmail.com>
2023-09-14 03:01:41 -0400

    added Game class

---
+++ MathGame/Game.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathGame
+{
+    internal class Game
+    {
+        internal int Score { get; set; }
+        internal int MaxScore { get; set; }
+        internal DateTime Date { get; set; }
+        internal GameType Type { get; set; }
+        internal Difficulty Difficulty { get; set; }
+    }
+
+    internal enum GameType
+    {
+        Addition,
+        Subtraction,
+        Multiplication,
+        Division
+    }
+
+    internal enum Difficulty
+    {
+        Easy,
+        Medium,
+        Hard
+    }
+}
